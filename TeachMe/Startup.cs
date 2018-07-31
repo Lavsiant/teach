cf20f3c332d5a248be1dc0f6bc588e3d235f20b9
Lavsiant <lavsiant0@gmail.com>
2018-07-31 02:56:52 -0700

    Commit all

--- TeachMe/Startup.cs
+++ TeachMe/Startup.cs
@@ -42,7 +42,7 @@ namespace TeachMe
                 options.Password.RequireUppercase = false;
                 options.Password.RequireLowercase = false;
                 options.Password.RequiredUniqueChars = 6;
-
+                //sadddddddddddd
                 // Lockout settings
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
                 options.Lockout.MaxFailedAccessAttempts = 10;
